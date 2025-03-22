39f27efffad0b5600aa58b32b560c96aaf1d53eb
teerakanotk <teerakan.otk@gmail.com>
2025-03-22 16:22:49 +0700

    fix: Remove old config file

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,3 @@
-// import nextra from "nextra";
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   // You can include other Next.js config options here
-// };
-
-// const withNextraConfig = nextra({
-//   // ... Other Nextra config options
-// });
-
-// export default withNextraConfig({
-//   ...nextConfig,
-//   // You can include additional Next.js config options here as well
-// });
-
 import { NextConfig } from "next";
 
 import nextra from "nextra";
